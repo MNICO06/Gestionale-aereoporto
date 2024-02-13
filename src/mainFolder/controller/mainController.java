21c9e879a5974cb502bde0eba6cf41e4e57e204b
Matteo <124249718+Teooo06@users.noreply.github.com>
2024-02-13 17:35:29 +0100

    Update mainController.java
    Aggiunta Orologio

--- src/mainFolder/controller/mainController.java
+++ src/mainFolder/controller/mainController.java
@@ -5,10 +5,35 @@
  */
 package mainFolder.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author molte
  */
 public class mainController {
+
+    @FXML private Label orologio;
+    @FXML private Label titolo;
+    @FXML private Button accedi;
+
+
+    public void initialize() {
+        // Creazione di un Timeline per aggiornare l'orologio ogni secondo
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+            orologio.setText(dateFormat.format(new Date()));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
     
 }
