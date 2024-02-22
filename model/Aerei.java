ccbc36d3f29efc4dcf4d46545a20b6f77bb030a0
MNICO06 <82870159+MNICO06@users.noreply.github.com>
2024-02-22 11:12:04 +0100

    Add files via upload

---
+++ model/Aerei.java
@@ -0,0 +1,223 @@
+package mainFolder.model;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Aerei {
+    private final String modelloAereo;
+    private StringProperty provenienza;
+    private StringProperty destinazione;
+    private final String compagniaAerea;
+    private final String codiceRegistrazione;
+
+    private final int numeroMassimoPasseggeri;
+    private IntegerProperty numeroPostiOccupati;
+    private IntegerProperty gate;
+    private IntegerProperty terminal;
+
+
+    private IntegerProperty ritardo;
+    private ObjectProperty<LocalDate> giornoDiArrivo;
+    private ObjectProperty<LocalTime> oraArrivo;
+    private ObjectProperty<LocalDate> giornoDiPartenza;
+    private ObjectProperty<LocalTime> oraPartenza;
+    private int intervalloDiGiorni;
+    // .plusDays(intervallo giorni)
+
+
+    DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    DateTimeFormatter formatterOre = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+
+    private boolean scaricoBagagli = false;
+    private boolean caricoBagagli = false;
+    private boolean imbarco = false;
+
+
+
+    public Aerei (String modello, String provenienza, String destinazione, String compagnia, String codice,
+         int numMax, LocalDate giornoDiArrivo, LocalTime oraArrivo, 
+          LocalDate giornoPartenza, LocalTime oraPartenza, int intervallo){
+
+        this.modelloAereo = modello;
+        this.provenienza = new SimpleStringProperty (provenienza);
+        this.destinazione = new SimpleStringProperty (destinazione);
+        this.compagniaAerea = compagnia;
+        this.codiceRegistrazione = codice;
+        this.numeroMassimoPasseggeri = numMax;
+        this.giornoDiArrivo = new SimpleObjectProperty<> (giornoDiArrivo);
+        this.oraArrivo = new SimpleObjectProperty<> (oraArrivo);
+        this.giornoDiPartenza = new SimpleObjectProperty<> (giornoPartenza);
+        this.oraPartenza = new SimpleObjectProperty<> (oraPartenza);
+        this.intervalloDiGiorni = intervallo;
+        ritardo.set(0);
+        
+    }
+
+    //---------------------------getter------------------------------------------------
+
+    public String getModello() {
+        return modelloAereo;
+    }
+
+    public StringProperty getProvenienzaProperty() {
+        return provenienza;
+    }
+    public String getProvenienzaString() {
+        return provenienza.get();
+    }
+
+    public StringProperty getDestinazioneProperty() {
+        return destinazione;
+    }
+    public String getDestinazioneString() {
+        return destinazione.get();
+    }
+
+    public String getCompagnia() {
+        return compagniaAerea;
+    }
+
+    public String getCodice() {
+        return codiceRegistrazione;
+    }
+
+    public int postiMassimi () {
+        return numeroMassimoPasseggeri;
+    }
+
+    public IntegerProperty getNumeroPostiOccupatiProperty() {
+        return numeroPostiOccupati;
+    }
+    public int getNumeroPostiOccupatiInt() {
+        return numeroPostiOccupati.get();
+    }
+
+    public IntegerProperty getGateProperty() {
+        return gate;
+    }
+    public int getGateInt() {
+        return gate.get();
+    }
+
+    public IntegerProperty getTerminalProperty() {
+        return terminal;
+    }
+    public int getTerminalInt() {
+        return terminal.get();
+    }
+
+    public IntegerProperty getRitardoProperty() {
+        return ritardo;
+    }
+    public int getRitardoInt() {
+        return ritardo.get();
+    }
+
+    public ObjectProperty<LocalDate> getGiornoArrivoProperty() {
+        return giornoDiArrivo;
+    }
+    public String getGiornoArrivoString() {
+        return giornoDiArrivo.get().format(formatterData);
+    }
+
+    public ObjectProperty<LocalTime> getOraArrivoProperty () {
+        return oraArrivo;
+    }
+    public String getOraArrivoString() {
+        return oraArrivo.get().format(formatterOre);
+    }
+
+    public ObjectProperty<LocalDate> getGiornoPartenzaProperty() {
+        return giornoDiPartenza;
+    }
+    public String getGiornoPartenzaString() {
+        return giornoDiPartenza.get().format(formatterData);
+    }
+
+    public ObjectProperty<LocalTime> getOraPartenzaProperty() {
+        return oraPartenza;
+    }
+    public String getOraPartenzaString() {
+        return oraPartenza.get().format(formatterOre);
+    }
+
+    public boolean getScaricoBagagli() {
+        return scaricoBagagli;
+    }
+
+    public boolean getCaricoBagagli() {
+        return caricoBagagli;
+    }
+
+    public boolean getImbarco() {
+        return imbarco;
+    }
+
+    //---------------------------setter-----------------------------------------
+
+    public void setProvenienza(String provenienzaString) {
+        this.provenienza.set(provenienzaString);
+    }
+
+    public void setDestinazione(String destinazioneString) {
+        this.destinazione.set(destinazioneString);
+    }
+
+    public void setNumeroPostiOccupati(int numeroPostiOccupati) {
+        this.numeroPostiOccupati.set(numeroPostiOccupati);
+    }
+
+    public void setGate(int gate) {
+        this.gate.set(gate);
+    }
+
+    public void setTerminal(int terminal) {
+        this.terminal.set(terminal);
+    }
+
+    public void setRitardo(int ritardo) {
+        this.ritardo.set(ritardo);
+    }
+
+    public void setGiornoArrivo(LocalDate giornoDiArrivo) {
+        this.giornoDiArrivo.set(giornoDiArrivo);
+    }
+
+    public void setOraArrivo(LocalTime oraArrivo) {
+        this.oraArrivo.set(oraArrivo);
+    }
+
+    public void setGiornoPartenza(LocalDate giornoDiPartenza) {
+        this.giornoDiPartenza.set(giornoDiPartenza);
+    }
+
+    public void setOraPartenza(LocalTime oraPartenza) {
+        this.oraPartenza.set(oraPartenza);
+    }
+
+    public void setScaricoBagagli(boolean scaricoBagagli) {
+        this.scaricoBagagli = scaricoBagagli;
+    }
+
+    public void setCaricoBagagli(boolean caricoBagagli) {
+        this.caricoBagagli = caricoBagagli;
+    }
+
+    public void setImbarco(boolean imbarco) {
+        this.imbarco = imbarco;
+    }
+
+    public void setIntervalloGiorni(int intervallo) {
+        this.intervalloDiGiorni = intervallo;
+    }
+
+    
+}
