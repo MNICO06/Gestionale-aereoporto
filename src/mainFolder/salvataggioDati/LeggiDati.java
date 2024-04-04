3d74aef08f03e94971f441706b9edffe0bbd667e
MNICO06 <82870159+MNICO06@users.noreply.github.com>
2024-04-04 09:59:30 +0200

    Update LeggiDati.java

--- src/mainFolder/salvataggioDati/LeggiDati.java
+++ src/mainFolder/salvataggioDati/LeggiDati.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import mainFolder.model.GestioneUtenti;
 import mainFolder.model.Utenti;
+
 
 public class LeggiDati {
 
@@ -14,29 +16,83 @@ public class LeggiDati {
 
     }
 
-    public void leggiUtente() {
+    public ArrayList<Utenti> leggiUtente() {
         try {
+            ArrayList<Utenti> listaUtenti = new ArrayList<Utenti>();
+            int numLinee = 0;
+
             //aprtura del file da scrivere
-            FileReader apertura = new FileReader("utenti.txt");
+            FileReader apertura = new FileReader("./src/mainFolder/salvataggioDati/utenti.txt");
             BufferedReader fileDaLeggere = new BufferedReader(apertura);
             String linea;
 
             ArrayList<String> datiArrayList = new ArrayList<>();
 
             while ((linea = fileDaLeggere.readLine()) != null){
+                numLinee++;
                 String [] dati = linea.split("\\+");
                 datiArrayList.addAll(Arrays.asList(dati));
             }
 
-            System.out.println(datiArrayList);
+            for (int i = 0; i < numLinee; i++) {
+                String nome = "";
+                String cognome = "";
+                String mail = "";
+                String nascita = "";
+                String password = "";
+                String numCell = "";
+                String nazione = "";
+                String citta = "";
+                String via = "";
+                String codice = "";
+                String scadenza = "";
+                for (int j = 0; j < 11; j++) {
+                    
+                    if (j == 0) {
+                        nome = datiArrayList.get(j);
+                    }
+                    if (j == 1) {
+                        cognome = datiArrayList.get(j);
+                    }
+                    if (j == 2) {
+                        mail = datiArrayList.get(j);
+                    }
+                    if (j == 3) {
+                        nascita = datiArrayList.get(j);
+                    }
+                    if (j == 4) {
+                        password = datiArrayList.get(j);
+                    }
+                    if (j == 5) {
+                        numCell = datiArrayList.get(j);
+                    }
+                    if (j == 6) {
+                        nazione = datiArrayList.get(j);
+                    }
+                    if (j == 7) {
+                        citta = datiArrayList.get(j);
+                    }
+                    if (j == 8) {
+                        via = datiArrayList.get(j);
+                    }
+                    if (j == 9) {
+                        codice = datiArrayList.get(j);
+                    }
+                    if (j == 10) {
+                        scadenza = datiArrayList.get(j);
+                    }
+                }
+                Utenti u = new Utenti(nome, cognome, mail, nascita, password, numCell, nazione, citta, via, codice, scadenza);
+                listaUtenti.add(u);
+            }
 
-            
-
-
+            return listaUtenti;
 
         }catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
+        
     }
 
     
