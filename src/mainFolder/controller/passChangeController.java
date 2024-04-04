bff438c5aca710fbdb42fa338ce6b6f0953d3d43
Matteo <124249718+Teooo06@users.noreply.github.com>
2024-04-04 09:05:06 +0200

    Update passChangeController.java

--- src/mainFolder/controller/passChangeController.java
+++ src/mainFolder/controller/passChangeController.java
@@ -1,9 +1,13 @@
 package mainFolder.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class passChangeController {
     @FXML private TextField inserisciMail;
@@ -11,6 +15,25 @@ public class passChangeController {
     @FXML private PasswordField confermaNuovaPassword;
     
     @FXML private Button cambiPassword;
+    @FXML private Button btnHome;
 
-    
+    // Metodo per il tasto Home
+    @FXML
+    private void handleBtnHome() {
+        try {
+            // Carica la seconda GUI (FXML)
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../guiFolder/userGui.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("User GUI");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            // Chiudi la prima GUI (Finestra)
+            Stage primaryStage = (Stage) btnHome.getScene().getWindow();
+            primaryStage.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
